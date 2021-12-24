e2b9ceb88f697ac2f5d42f007c68c736a914b3b4
vandana-eva <92171611+vandana-eva@users.noreply.github.com>
2021-12-24 18:38:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+var canvas = new fabric.Canvas('myCanvas');
+ball_x = 10;
+ball_y = 10;
+var ball_object = "";
+var golf_image_object="";
+function ball_update()
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	
+	new_image();
+}
+
+function new_image()
+{
+	// write code to Upload ball image on canvas
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	/* Check the coordinates of the ball and hole images to finish the game. 
+	And id coordinates matches them remove ball image, 
+	display "GAME OVER!!!" 
+	and make canvas border 'red'. */
+	
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		// Write a code to move ball upward.
+	}
+
+	function down()
+	{
+		 // Write a code to move ball downward.
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			// Write a code to move ball left side.
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			// Write a code to move ball right side.
+		}
+	}
+	
+}
+
